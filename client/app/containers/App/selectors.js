2d0406da9d5913f400eb28a08530ffb00fb1d2c1
yirideveloper <topyiri1113@gmail.com>
2018-10-08 12:56:33 +0545

    make port of client 5001

--- client/app/containers/App/selectors.js
+++ client/app/containers/App/selectors.js
@@ -1,33 +1,8 @@
 import { createSelector } from 'reselect';
 
 const selectRoute = state => state.get('route');
-const selectGlobal = state => state.get('global');
 
 const makeSelectLocation = () =>
   createSelector(selectRoute, routeState => routeState.get('location').toJS());
 
-const makeSelectIsAuthenticated = () =>
-  createSelector(selectGlobal, state => !!state.get('token'));
-const makeSelectToken = () =>
-  createSelector(selectGlobal, state => state.get('token'));
-const makeSelectUser = () =>
-  createSelector(selectGlobal, state => state.get('user'));
-const makeSelectDialog = () =>
-  createSelector(selectGlobal, state => state.get('dialog'));
-const makeSelectRequesting = () =>
-  createSelector(selectGlobal, state => state.get('requesting'));
-const makeSelectSuccess = () =>
-  createSelector(selectGlobal, state => state.get('success'));
-const makeSelectMessages = () =>
-  createSelector(selectGlobal, state => state.get('messages'));
-
-export {
-  makeSelectLocation,
-  makeSelectIsAuthenticated,
-  makeSelectToken,
-  makeSelectUser,
-  makeSelectDialog,
-  makeSelectRequesting,
-  makeSelectSuccess,
-  makeSelectMessages,
-};
+export { makeSelectLocation };
--- client/app/reducers.js
+++ client/app/reducers.js
@@ -7,7 +7,6 @@ import { fromJS } from 'immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
-import globalReducer from './containers/App/reducer';
 
 /*
  * routeReducer
@@ -44,7 +43,6 @@ export default function createReducer(injectedReducers) {
   return combineReducers({
     route: routeReducer,
     language: languageProviderReducer,
-    global: globalReducer,
     ...injectedReducers,
   });
 }
