8a16b4bb6ec6019641b4d97d8ba30bf121179bc2
yirideveloper <topyiri1113@gmail.com>
2018-10-11 17:46:15 +0545

    update

--- client/app/containers/HomePage/SearchComponent/constants.js
+++
@@ -1,3 +0,0 @@
-export const LOAD_CATEGORIES_REQUEST = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_REQUEST';
-export const LOAD_CATEGORIES_SUCCESS = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_SUCCESS';
-export const LOAD_CATEGORIES_FAILURE = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_FAILURE';
