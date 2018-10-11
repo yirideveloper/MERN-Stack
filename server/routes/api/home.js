d86952302bd37a2a9b61cbf0d114159dc8d02924
yirideveloper <topyiri1113@gmail.com>
2018-10-11 11:36:17 +0545

    Api fopr home completed

---
+++ server/routes/api/home.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const dModule = require('../../modules/Home/HomeController');
+
+router.get('/category', dModule.GetCategories);
+router.get('/cat/:catid', dModule.GetOrganizationByCategoryById);
+router.get('/org/:orgslug', dModule.GetOrganizationDetailByslug);
+
+module.exports = router;
