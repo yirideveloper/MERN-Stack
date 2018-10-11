ff5d988110a498a89385164d7d10f1905a8b7be5
yirideveloper <topyiri1113@gmail.com>
2018-10-11 11:47:57 +0545

    a block added

--- server/config/email.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    protocal: 'SMTP',
-    email: 'test@mkmpvtltd.tk',
-    password:'CfhUAqwhZCwT7c5HuP',
-    server:'mx1.hostinger.co.uk',
-    port:'587',
-    security:'STARTTLS'
-  };
-  
