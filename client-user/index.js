4286fe61967dea4a666ac38bb567573b3bea6139
yirideveloper <topyiri1113@gmail.com>
2018-11-28 13:43:27 +0545

    update message for individual server

--- client-user/index.js
+++ client-user/index.js
@@ -1,15 +1,13 @@
-'use strict';
-
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 5010;
+const port = process.env.PORT || 5012;
 const server = http.createServer(app);
 
 app.set('PORT_NUMBER', port);
 
 //  Start the app on the specific interface (and port).
 server.listen(port, () => {
-  console.log(`API Server started on port ${port} at Date ${new Date()}`);
+  console.log(`Design Server started on port ${port} at Date ${new Date()}`);
 });
 
 process.on('SIGTERM', () => {
