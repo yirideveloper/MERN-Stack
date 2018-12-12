5f6136992ec3f86a33f5929523854682c91bfd83
yirideveloper <topyiri1113@gmail.com>
2018-12-12 15:43:24 +0545

    validation and sanitization library creation

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,20 +19,11 @@ router.use('/module', dmodule);
 // All route of Static Data from DB Module
 const staticRoutes = require('./api/static');
 router.use('/static', staticRoutes);
-//All route of Blog 
+// All routes of blog here
 const blogRoutes = require('./api/blog');
 router.use('/blog', blogRoutes);
-//All route of registration
-const registrationRoutes = require('./api/registration');
-router.use('/registration', registrationRoutes);
-
-// For word press
-const wordpressRoutes = require('./api/wordpress_schema');
-router.use('/wordpress', wordpressRoutes);
-
-// For Designations
-const DesignationRoutes = require('./api/Designation');
-router.use('/Designation', DesignationRoutes);
-
+// All routes of fiscals here
+const fiscalRoutes = require('./api/fiscal');
+router.use('/fiscal', fiscalRoutes);
 
 module.exports = router;
