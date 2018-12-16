ffd45d38568abb7eee64c747105659e5b248556b
yirideveloper <topyiri1113@gmail.com>
2018-12-16 15:07:33 +0545

    Seaching option is added

--- server/routes/api/Designation.js
+++ server/routes/api/Designation.js
@@ -3,43 +3,20 @@ const router = express.Router();
 
 const dModule = require("../../modules/Designation/DesignationController");
 
-const Validator = require("validator");
-const isEmpty = require("../../validation/isEmpty");
+const validator = require('validator');
+const isEmpty = require('../../validation/isEmpty');
 
-const HttpStatus = require("http-status");
-const OtherHelper = require("../../helper/others.helper");
+const HttpStatus = require('http-status');
+const OtherHelper = require('../../helper/others.helper');
 
-router.get("/", dModule.GetDesignation);
+const DesignationValidation = require('./../../modules/Designation/DesignationValidation');
 
-router.get("/:id", dModule.GetDesignationDetail);
+router.get('/', dModule.GetDesignation);
 
-router.post("/", validateDesignationInput, dModule.AddDesignation);
+router.get('/:id', dModule.GetDesignationDetail);
 
-router.delete("/:id", dModule.DeleteDesignation);
+router.post('/', DesignationValidation.Designation, dModule.AddDesignation);
 
-function validateDesignationInput(req, res, next) {
-  const data = req.body;
-  let errors = {};
-
-  data.Designation = !isEmpty(data.Designation) ? data.Designation : "";
-
-  !validator.isEmpty(data.Designation)
-    ? (errors.Designation = "Designation is invalid")
-    : null;
-
-  if (!isEmpty(errors)) {
-    return otherHelper.sendResponse(
-      res,
-      HttpStatus.BAD_REQUEST,
-      false,
-      null,
-      errors,
-      "validation Error.",
-      null
-    );
-  } else {
-    next();
-  }
-}
+router.delete('/:id', dModule.DeleteDesignation);
 
 module.exports = router;
