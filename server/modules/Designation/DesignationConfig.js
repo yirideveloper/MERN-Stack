10b3d34b03c0f0657c809a189edd6320d8503714
yirideveloper <topyiri1113@gmail.com>
2018-12-19 16:42:00 +0545

    logo change

--- server/modules/Designation/DesignationConfig.js
+++ server/modules/Designation/DesignationConfig.js
@@ -2,5 +2,7 @@ module.exports = {
   validationMessage: {
     DesignationRequired: ' Designation must be entered',
     DesignationInvalidlength: 'Designation must be atleast 5 characters, maximum limit is 10',
+    PhoneRequired: 'Phone number must be entered',
+    PhoneInvalidlength: 'Please enter valid phone number',
   },
 };
