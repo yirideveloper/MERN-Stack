7c0d6f1ae70131db15fa707bea83b857670d58bd
yirideveloper <topyiri1113@gmail.com>
2018-12-25 15:42:37 +0545

    designation and leaveType Page Edited

--- server/modules/Roles/role.js
+++ server/modules/Roles/role.js
@@ -6,7 +6,7 @@ const RolesSchema = new Schema({
   Description: { type: String, required: false },
   IsActive: { type: Boolean, required: true, default: true },
   Added_at: { type: Date, default: Date.now },
-  Added_by: { type: Schema.Types.ObjectId, ref: 'users' },
+  Added_by: { type: Schema.Types.ObjectId },
 });
 
 module.exports = Roles = mongoose.model('roles', RolesSchema);
