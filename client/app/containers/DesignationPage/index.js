c3a2653773cc7c76d961b5dbdad57009e8e74ff9
yirideveloper <topyiri1113@gmail.com>
2019-01-02 12:13:28 +0545

    IsFloat added

--- client/app/containers/DesignationPage/index.js
+++ client/app/containers/DesignationPage/index.js
@@ -29,7 +29,7 @@ import injectReducer from "../../utils/injectReducer";
 import reducer from "./reducer";
 import saga from "./saga";
 import { loadAllRequest, deleteOneRequest } from "./actions";
-import { makeSelectAll, makeSelectPage } from "./selectors";
+import { makeSelectAll } from "./selectors";
 import { FormattedMessage } from "react-intl";
 import messages from "./messages";
 
@@ -93,11 +93,7 @@ export class DesignationPage extends React.Component {
     // this.props.history.push(`/wt/link-manage/edit/${id}`);
   };
   handleSearch = () => {
-    this.props.loadAll({
-      query: this.state.query,
-      page: this.state.page,
-      rowsPerPage: this.state.rowsPerPage
-    });
+    this.props.loadAll({ query: this.state.query });
     this.setState({ query: {} });
   };
 
@@ -107,41 +103,14 @@ export class DesignationPage extends React.Component {
     } else if (!this.state.sortToggle) {
       this.setState({ sortToggle: 1, sortSymbol: "A" });
     }
-    this.props.loadAll({
-      sort: `${this.state.sortToggle}${title}`,
-      page: this.state.page,
-      rowsPerPage: this.state.rowsPerPage
-    });
+    this.props.loadAll({ sort: `${this.state.sortToggle}${title}` });
   };
-
-  //Pagination
-  handleChangePage = (event, page) => {
-    this.setState({ page: page + 1 }, () => {
-      this.props.loadAll({
-        page: this.state.page,
-        rowsPerPage: this.state.rowsPerPage
-      });
-    });
-  };
-  handleChangeRowsPerPage = event => {
-    this.setState({ rowsPerPage: event.target.value }, () => {
-      this.props.loadAll({
-        // page: this.state.page,
-        rowsPerPage: this.state.rowsPerPage
-      });
-    });
-  };
-
   render() {
-    const { classes, allLinks, pageItem } = this.props;
+    const { classes, allLinks } = this.props;
     const allLinksObj = allLinks.toJS();
-    const pageObj = pageItem.toJS();
-
-    const { page = 1, size = 10, totaldata = 20 } = pageObj;
     const tableData = allLinksObj.map(
-      ({ Designation, DesignationNepali, update_date, IsActive, _id }) => [
+      ({ Designation, update_date, IsActive, _id }) => [
         Designation,
-        DesignationNepali,
         moment(update_date).format("MMM Do YY"),
         IsActive,
         <React.Fragment>
@@ -226,7 +195,6 @@ export class DesignationPage extends React.Component {
                       </span>
                     )}
                   </FormattedMessage>,
-                  <FormattedMessage {...messages.designationNepali} />,
                   <FormattedMessage {...messages.updateDate}>
                     {txt => (
                       <span onClick={() => this.designationSort("update_date")}>
@@ -236,11 +204,6 @@ export class DesignationPage extends React.Component {
                   </FormattedMessage>
                 ]}
                 tableData={tableData}
-                page={page}
-                size={size}
-                totaldata={totaldata}
-                handleChangePage={this.handleChangePage}
-                handleChangeRowsPerPage={this.handleChangeRowsPerPage}
               />
               <Button
                 variant="fab"
@@ -265,8 +228,7 @@ DesignationPage.propTypes = {
 };
 
 const mapStateToProps = createStructuredSelector({
-  allLinks: makeSelectAll(),
-  pageItem: makeSelectPage()
+  allLinks: makeSelectAll()
 });
 
 const mapDispatchToProps = dispatch => ({
