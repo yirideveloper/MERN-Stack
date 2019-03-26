aae0f76faec0d72c00ec346753fce732db349bff
yirideveloper <topyiri1113@gmail.com>
2019-03-26 15:06:03 +0545

    mix changes

--- client-v2/app/containers/App/saga.js
+++ client-v2/app/containers/App/saga.js
@@ -1,8 +1,7 @@
-import { call, takeEvery, takeLatest, select } from 'redux-saga/effects';
+import { call, takeEvery } from 'redux-saga/effects';
 import Api from 'utils/Api';
 import * as types from './constants';
 import * as actions from './actions';
-import { makeSelectToken } from './selectors';
 
 function* loadContent(action) {
   yield call(
@@ -22,15 +21,8 @@ function* loadMedia(action) {
     ),
   );
 }
-function* logOut() {
-  const token = yield select(makeSelectToken());
-  yield call(
-    Api.get(`user/logout`, actions.logoutSuccess, actions.logoutFailure, token),
-  );
-}
 
 export default function* defaultSaga() {
   yield takeEvery(types.LOAD_CONTENT_REQUEST, loadContent);
   yield takeEvery(types.LOAD_MEDIA_REQUEST, loadMedia);
-  yield takeLatest(types.LOGOUT_REQUEST, logOut);
 }
