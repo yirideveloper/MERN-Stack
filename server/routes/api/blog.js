22dfdfebff7462bd30cf3b40cd62ee8f767c3cb7
yirideveloper <topyiri1113@gmail.com>
2019-08-27 13:32:08 +0545

    codes of api creation for comment section of blog

--- server/routes/api/blog.js
+++ server/routes/api/blog.js
@@ -10,6 +10,7 @@ const { catSanitize, catValidate, sanitize, validate } = require('../../modules/
 router.get('/auth', authorization, authentication, blogModule.GetBlogAuthorize);
 router.get('/', blogModule.GetBlogUnauthorize);
 router.get('/latest', blogModule.getLatestBlog);
+router.get('/latest/:cat_id', blogModule.getLatestBlogByCat);
 router.get('/related/:slug_url', blogModule.getRealtedBlog);
 router.get('/category', blogModule.GetBlogCategory);
 router.get('/category/:slug', blogModule.GetBlogCatBySlug);
@@ -19,9 +20,13 @@ router.get('/blogbycat/:id', blogModule.GetBlogByCat);
 router.get('/blogbytag/:tag', blogModule.GetBlogByTag);
 router.get('/blogbyauthor/:author', blogModule.GetBlogByAuthor);
 router.get('/blogbytime/:time', blogModule.GetBlogByDate);
+router.get('/comment', blogModule.GetBlogComment);
+router.get('/comment/:blog', blogModule.GetBlogCommentByBlog);
+router.post('/comment', authorization, blogModule.PostBlogComment);
 router.post('/', authorization, authentication, uploader.single('file'), sanitize, validate, blogModule.SaveBlog);
 router.post('/category', authorization, authentication, uploader.single('file'), catSanitize, catValidate, blogModule.SaveBlogCategory);
 router.delete('/:id', authorization, authentication, blogModule.DeleteBlog);
 router.delete('/category/:id', authorization, authentication, blogModule.DeleteBlogCat);
+router.delete('/comment/:id', authorization, blogModule.DeleteBlogComment);
 
 module.exports = router;
