6e815288e12d619966521d290ad6a386f0600f0b
yirideveloper <topyiri1113@gmail.com>
2019-08-28 12:47:54 +0545

    Sim link for docs

--- client/app/containers/BlogPage/reducer.js
+++ client/app/containers/BlogPage/reducer.js
@@ -8,10 +8,6 @@ export const initialState = {
   relatedBlogsIsLoading: false,
   recentBlogs: [],
   recentBlogsIsLoading: false,
-  one: {
-    comment: '',
-  },
-  comments: {},
 };
 
 /* eslint-disable default-case, no-param-reassign */
@@ -47,12 +43,6 @@ const blogPageReducer = (state = initialState, action) =>
         break;
       case types.LOAD_RECENT_BLOGS_FAILURE:
         draft.recentBlogsIsLoading = false;
-        break;
-      case types.SET_ONE_VALUE:
-        draft.one[action.payload.key] = action.payload.value;
-        break;
-      case types.LOAD_COMMENT_SUCCESS:
-        draft.comments = action.payload.data;
         break;
     }
   });
