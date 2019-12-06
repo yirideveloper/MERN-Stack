5339d883d01a5044cc481809c044a499a99b3333
yirideveloper <topyiri1113@gmail.com>
2019-12-06 12:39:28 +0545

    sanitization fixed

--- server/routes/api/menu.js
+++ server/routes/api/menu.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { menuController, menuItemController } = require('../../modules/menu/menucontroller');
-const { sanitize, validate, itemsanitize, itemvalidate } = require('../../modules/menu/menuValidation');
+const {menuController , menuItemController} = require('../../modules/menu/menucontroller');
+const {sanitize,validate} = require('../../modules/menu/menuValidation');
 const { authorization } = require('../../middleware/authentication.middleware');
 
-router.get('/', menuController.getMenu);
+router.get('/' , menuController.getMenu);
 
-router.post('/', sanitize, validate, authorization, menuController.saveMenu);
+router.post('/' ,sanitize , validate,authorization, menuController.saveMenu);
 
-router.delete('/:id', authorization, menuController.deleteMenu);
-router.get('/menuitem', menuItemController.getMenuItem);
+router.get('/:id' , menuController.getMenuDetail);
 
-router.get('/:id', menuController.getEditMenu);
+router.delete('/:id' ,authorization, menuController.deleteMenu);
 
-router.post('/menuitem', itemsanitize, authorization, menuItemController.saveMenuItem); //itemvalidate,
+
+
+
 
 module.exports = router;
