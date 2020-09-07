39c9c4a254ecddde116bde89df9322c18083164a
yirideveloper <topyiri1113@gmail.com>
2020-09-07 12:49:53 +0545

    updates

--- client/app/containers/Admin/Module/reducer.js
+++ client/app/containers/Admin/Module/reducer.js
@@ -18,7 +18,6 @@ export const initialState = {
     module_name: '',
     description: '',
     path: [],
-    module_group: '',
   },
   access: {
     Access: [],
@@ -30,7 +29,6 @@ export const initialState = {
   query: { find_module_name: '' },
   loading: false,
   errors: { module_name: '', description: '' },
-  sub_module: [],
 };
 
 /* eslint-disable default-case, no-param-reassign */
@@ -68,7 +66,7 @@ const adminRoleReducer = (state = initialState, action) =>
         break;
       case types.LOAD_ONE_SUCCESS:
         draft.loading = false;
-        draft.one = { ...initialState.one, ...action.payload.data };
+        draft.one = action.payload.data;
         break;
       case types.LOAD_ONE_FAILURE:
         draft.loading = false;
@@ -82,10 +80,6 @@ const adminRoleReducer = (state = initialState, action) =>
         break;
       case types.SET_QUERY_VALUE:
         draft.query[action.payload.key] = action.payload.value;
-        break;
-
-      case types.LOAD_SUB_MODULE_SUCCESS:
-        draft.sub_module = action.payload.data;
         break;
     }
   });
