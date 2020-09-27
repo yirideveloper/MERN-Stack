f7533722ee77f04fc207b971436ba187042f33eb
yirideveloper <topyiri1113@gmail.com>
2020-09-27 14:31:22 +0545

    update

--- client/app/containers/Admin/Role/selectors.js
+++ client/app/containers/Admin/Role/selectors.js
@@ -5,7 +5,8 @@ import { initialState } from './reducer';
  * Direct selector to the adminRole state domain
  */
 
-export const selectAdminRoleDomain = state => state.adminRole || initialState;
+export const selectAdminRoleDomain = state =>
+  state.adminRole || initialState;
 
 /**
  * Other specific selectors
@@ -39,24 +40,6 @@ export const makeSelectErrors = () =>
   createSelector(
     selectAdminRoleDomain,
     state => state.errors,
-  );
-
-export const makeSelectModuleData = () =>
-  createSelector(
-    selectAdminRoleDomain,
-    state => state.module_data,
-  );
-
-export const makeSelectRoleData = () =>
-  createSelector(
-    selectAdminRoleDomain,
-    state => state.role_data,
-  );
-
-export const makeSelectLoaders = () =>
-  createSelector(
-    selectAdminRoleDomain,
-    state => state.loaders,
   );
 
 /**
