e3466f83ce1055346d186cb36160726323cb50fd
yirideveloper <topyiri1113@gmail.com>
2021-02-04 12:25:24 +0545

    small bug fixes

--- client/app/containers/Comments/index.js
+++ client/app/containers/Comments/index.js
@@ -80,7 +80,7 @@ export const Comments = props => {
   };
 
   return commentLoading ? (
-    <>Loading...</>
+    <div>Loading....</div>
   ) : (
       <div>
         <h2 className="mt-4" htmlFor="comment">
