6dc86c59eb8a0e55b618a8a769c29fa0915fa155
yirideveloper <topyiri1113@gmail.com>
2021-03-04 13:00:27 +0545

    delete done

--- client/app/containers/EditorFileSelect/components/FileList.js
+++ client/app/containers/EditorFileSelect/components/FileList.js
@@ -9,7 +9,7 @@ import Dropzone from 'react-dropzone';
 
 import Dialog from '../../../components/Dialog/index';
 import PageContent from '../../../components/PageContent/PageContent';
-import { FaCheck } from 'react-icons/fa';
+import { FaCheck, FaFolderOpen } from 'react-icons/fa';
 
 import * as mapDispatchToProps from '../actions';
 import {
@@ -39,7 +39,6 @@ import {
   FaSearch,
   FaEdit,
 } from 'react-icons/fa';
-import { all } from 'redux-saga/effects';
 
 const LinkComponent = ({ children, staticContext, ...props }) => (
   <div {...props}>{children}</div>
@@ -97,8 +96,6 @@ const FileList = ({
   const [folderCheckbox, setfolderCheckbox] = useState(false);
   const [fileCheckbox, setfileCheckbox] = useState(false);
   const [selectedButton, setSelectedButton] = useState('');
-
-  const [dialogOpen, setDialogOpen] = useState(false);
 
   useEffect(() => {
     if (!folderAdded) {
@@ -362,14 +359,6 @@ const FileList = ({
     deleteMultipleRequest();
   };
 
-  const handleDialogOpen = () => {
-    setDialogOpen(true);
-  };
-
-  const handleDialogClose = () => {
-    setDialogOpen(false);
-  };
-
   return (
     <PageContent loading={loading}>
       <Dialog
@@ -402,23 +391,6 @@ const FileList = ({
               disabled={folderAdded}
             >
               Save
-            </button>
-          </>
-        }
-      />
-
-      <Dialog
-        open={dialogOpen}
-        onClose={handleDialogClose}
-        title={`Cant Ulpoad here`}
-        body={`Create sub folder first and then ony you can upload image`}
-        actions={
-          <>
-            <button
-              onClick={handleDialogClose}
-              className="block btn margin-none text-white bg-red-500 border border-red-600 hover:bg-red-600 mr-1"
-            >
-              Close
             </button>
           </>
         }
@@ -464,29 +436,18 @@ const FileList = ({
             </button>
           )} */}
 
-          {window.location.pathname.includes('media-manage') &&
-          self.name === 'root' ? (
-            <div
-              onClick={() => handleDialogOpen()}
-              className="items-center flex btn text-green-500 bg-green-100 border border-green-200 hover:bg-green-500 hover:border-green-500 mr-2 hover:text-white cursor-pointer"
-            >
-              <FaImage className="text-base mr-2" />
-              <span>Choose File</span>
-            </div>
-          ) : (
-            <Dropzone onDrop={file => handleFileUpload(file, self._id)}>
-              {({ getRootProps, getInputProps }) => (
-                <div
-                  className="items-center flex btn text-green-500 bg-green-100 border border-green-200 hover:bg-green-500 hover:border-green-500 mr-2 hover:text-white cursor-pointer"
-                  {...getRootProps()}
-                >
-                  <input {...getInputProps()} />
-                  <FaImage className="text-base mr-2" />
-                  <span>Choose File</span>
-                </div>
-              )}
-            </Dropzone>
-          )}
+          <Dropzone onDrop={file => handleFileUpload(file, self._id)}>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                className="items-center flex btn text-green-500 bg-green-100 border border-green-200 hover:bg-green-500 hover:border-green-500 mr-2 hover:text-white cursor-pointer"
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <FaImage className="text-base mr-2" />
+                <span>Choose File</span>
+              </div>
+            )}
+          </Dropzone>
           <button
             onClick={handleAdd}
             className="items-center flex btn text-blue-500 bg-blue-100 border border-blue-200 hover:bg-blue-500 hover:border-blue-500 mr-2 hover:text-white"
@@ -502,23 +463,23 @@ const FileList = ({
             <span>Rename</span>
           </button>
           {selectedButton === 'Delete' &&
-          (chosen_files.length > 0 || chosen_folders.length > 0) ? (
-            <button
-              onClick={confirmDelete}
-              className="blink items-center flex btn bg-red-100 border border-red-200 text-red-500 hover:bg-red-500 hover:border-red-500 hover:text-white"
-            >
-              <FaTrash className="text-base mr-2" />
-              <span>Confirm Delete</span>
-            </button>
-          ) : (
-            <button
-              onClick={handleDeleteButton}
-              className="items-center flex btn bg-red-100 border border-red-200 text-red-500 hover:bg-red-500 hover:border-red-500 hover:text-white"
-            >
-              <FaTrash className="text-base mr-2" />
-              <span>Delete</span>
-            </button>
-          )}
+            (chosen_files.length > 0 || chosen_folders.length > 0) ? (
+              <button
+                onClick={confirmDelete}
+                className="blink items-center flex btn bg-red-100 border border-red-200 text-red-500 hover:bg-red-500 hover:border-red-500 hover:text-white"
+              >
+                <FaTrash className="text-base mr-2" />
+                <span>Confirm Delete</span>
+              </button>
+            ) : (
+              <button
+                onClick={handleDeleteButton}
+                className="items-center flex btn bg-red-100 border border-red-200 text-red-500 hover:bg-red-500 hover:border-red-500 hover:text-white"
+              >
+                <FaTrash className="text-base mr-2" />
+                <span>Delete</span>
+              </button>
+            )}
         </div>
       </div>
       <div className="my-auto">
@@ -648,9 +609,8 @@ const FileList = ({
               </div>
               <div
                 // data-tooltip={each.name}
-                className={`${
-                  selected === each._id ? 'folder_media' : ''
-                } flex flex-col w-full h-36 text-center cursor-pointer overflow-hidden mt-10`}
+                className={`${selected === each._id ? 'folder_media' : ''
+                  } flex flex-col w-full h-36 text-center cursor-pointer overflow-hidden mt-10`}
                 onClick={() => handleSingleClick(each._id)}
                 onDoubleClick={() => handleFolderLink(each._id)}
                 onKeyDown={() => handleFolderLink(each._id)}
@@ -722,9 +682,8 @@ const FileList = ({
               </div>
               <div
                 // data-tooltip={each.filename}
-                className={`${
-                  selected === each._id ? 'folder_media' : ''
-                } flex flex-col w-full h-36 text-center cursor-pointer overflow-hidden mt-10`}
+                className={`${selected === each._id ? 'folder_media' : ''
+                  } flex flex-col w-full h-36 text-center cursor-pointer overflow-hidden mt-10`}
               >
                 <div className="flex">
                   <img
@@ -743,8 +702,9 @@ const FileList = ({
           </div>
         ))}
         {folders.data.length < 1 && files.data.length < 1 && (
-          <div className="text-center w-full text-sm h-64">
-            This Folder is Empty
+          <div className="h-64 flex items-center justify-center flex-col w-full">
+            <FaFolderOpen style={{ fontSize: '6rem' }} className="mb-5 opacity-10 mx-auto" />
+            <p className="text-gray-400">This folder is empty.</p>
           </div>
         )}
       </div>
