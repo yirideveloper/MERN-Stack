3b5f76c8611680643f1f4f516cf80b61d307a86b
yirideveloper <topyiri1113@gmail.com>
2022-02-04 16:01:05 +0545

    media

--- server/modules/contactUs/contactController.js
+++ server/modules/contactUs/contactController.js
@@ -15,7 +15,6 @@ contactController.PostContact = async (req, res, next) => {
     let contact_to_admin = await getSetting('template', 'email', 'contact_to_admin');
     let contact_to_user = await getSetting('template', 'email', 'contact_to_user');
     let admin_emails = await getSetting('user', 'admin_email', 'email_array');
-
     if (user) {
       const data = {
         name: user.name,
@@ -25,13 +24,13 @@ contactController.PostContact = async (req, res, next) => {
       };
       if (admin_emails.length != -1) {
         for (i = 0; i < admin_emails.length; i++) {
-          let renderedMail = await renderMail.renderTemplate(contact_to_admin, data, admin_emails[i]);
+          const renderedMail = await renderMail.renderTemplate(contact_to_admin, data, admin_emails[i]);
           if (renderMail.error) {
             console.log('render mail error: ', renderMail.error);
           } else {
             emailHelper.send(renderedMail, next);
           }
-        }
+        };
       }
       const renderedMailForAdmin = await renderMail.renderTemplate(contact_to_user, data, user.email);
       if (renderMail.error) {
